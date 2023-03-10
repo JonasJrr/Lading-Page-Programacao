2d1f122992d10b8b2e5ce9f31c72da09bc68dc31
Jonas Júnior <jonas.reis@shopdelivery.com.br>
2023-03-10 14:35:59 -0300

    correções

--- script.js
+++ script.js
@@ -3,31 +3,31 @@ let light = document.getElementById("changel");
 let def  = document.getElementById("default");
 
 dark.addEventListener("click", function(){
-    let body = document.getElementsByTagName("body")[0];
-    body.style.backgroundImage = "none";
-    body.style.backgroundColor = "black";
-    $(".tema").css("color", "white");
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "black";
     $(".teme").css("text-shadow", "3px 3px black");
     $(".teme").css("color", "white");
+    $(".tema").css("color", "white");
  });
 
 light.addEventListener("click", function(){
-    let body = document.getElementsByTagName("body")[0];
-    body.style.backgroundImage = "none";
-    body.style.backgroundColor = "white";
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "white";
     $(".tema").css("color", "black");
     $(".tema").css("text-shadow", "2px 1px blueviolet");
+    $(".tema").css("transition-duration", "500ms");
     $(".teme").css("color", "black");
     $(".teme").css("text-shadow", "1px 1px white");
-    $(".tema").css("transition-duration", "500ms");
+    $(".teme").css("transition-duration", "500ms");
+    $("body").css("transition-duration", "500ms");
  }); 
 
 def.addEventListener("click", function(){
-    let body = document.getElementsByTagName("body")[0];
-    body.style.backgroundImage = "url(./img/fundo-do-código-binário-wallpaper-digital-papel-de-parede-mundial-com-cores-roxas-cor-rosa-vermelha-226184723.jpg)";
-    $(".tema").css("color", "white");
+    document.body.style.backgroundColor = "#e62293";
+    document.body.style.backgroundImage = "url(./img/fundo-do-código-binário-wallpaper-digital-papel-de-parede-mundial-com-cores-roxas-cor-rosa-vermelha-226184723.jpg)";
     $(".teme").css("text-shadow", "3px 3px black");
     $(".teme").css("color", "white");
+    $(".tema").css("color", "white");
  });
 
  $('.header a[href^="#"]').on('click', function(e) { 
